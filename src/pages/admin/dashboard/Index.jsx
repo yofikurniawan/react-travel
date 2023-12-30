71809b129dc5c2d7d14c727739ebfc911938214a
Yofi Kurniawan <k.yofi.ky@gmail.com>
2023-12-30 11:47:21 +0700

    Penambahan Sidebar, Layouts Admin, dan Dashboard

---
+++ src/pages/admin/dashboard/Index.jsx
@@ -0,0 +1,28 @@
+// import react
+import React from "react";
+
+// import layout admin
+import LayoutAdmin from "../../layouts/Admin";
+
+function Dashboard() {
+
+  // title page
+  document.title = "Dashboard - Administrator Travel GIS";
+
+  return (
+    <React.Fragment>
+      <LayoutAdmin>
+        <div className="row mt-4">
+            <div className="col-12">
+                <div className="card border-0 rounded shadow-sm border-top-success">
+                    <div className="card-header">
+                        <span className="font-weight-bold"><i className="fa fa-tachometer-alt"></i> Dashboard</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </LayoutAdmin>
+    </React.Fragment>
+  );
+
+}
